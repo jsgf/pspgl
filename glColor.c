cbe1762570de71f1273be97fcd6cd6b985c8eee9
holger <none@none>
2005-09-24 16:27:16 +0000

    glColor3ub(), glColor4ub()

--- glColor.c
+++ glColor.c
@@ -9,12 +9,10 @@ void glColor4fv (const GLfloat *color)
 
 void glColor4f (GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)
 {
-	unsigned long c = ((((int) (255.0 * CLAMPF(alpha))) << 24) |
-			   (((int) (255.0 * CLAMPF(blue))) << 16) |
-			   (((int) (255.0 * CLAMPF(green))) << 8) |
-			    ((int) (255.0 * CLAMPF(red))));
-
-	pspgl_curctx->current.color = c;
+	pspgl_curctx->current.color = ((((int) (255.0 * CLAMPF(alpha))) << 24) |
+				       (((int) (255.0 * CLAMPF(blue))) << 16) |
+				       (((int) (255.0 * CLAMPF(green))) << 8) |
+					((int) (255.0 * CLAMPF(red))));
 }
 
 
@@ -27,5 +25,17 @@ void glColor3f (GLfloat red, GLfloat green, GLfloat blue)
 void glColor4ubv (const GLubyte *c)
 {
 	pspgl_curctx->current.color = (c[3] << 24) | (c[2] << 16) | (c[1] << 8) | c[0];
+}
+
+
+void glColor3ub (GLubyte r, GLubyte g, GLubyte b)
+{
+	pspgl_curctx->current.color = 0xff000000 | (b << 16) | (g << 8) | b;
+}
+
+ 
+void glColor4ub (GLubyte r, GLubyte g, GLubyte b, GLubyte a)
+{
+	pspgl_curctx->current.color = (a << 24) | (b << 16) | (g << 8) | b;
 }
 
