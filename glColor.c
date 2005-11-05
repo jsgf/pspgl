e4394d12a7f0dca3fbf619b01e8279b574d3d31c
jsgf <none@none>
2005-11-05 15:52:57 +0000

    Set the ambient material colour so that vertices without per-vertex colour will be right.

--- glColor.c
+++ glColor.c
@@ -1,18 +1,24 @@
 #include "pspgl_internal.h"
 
+static void update_color(unsigned long c)
+{
+	pspgl_curctx->current.color = c;
+	sendCommandi(CMD_MATERIAL_AMB_C, c);
+	sendCommandi(CMD_MATERIAL_AMB_A, c>>24);
+}
 
 void glColor4fv (const GLfloat *color)
 {
-	pspgl_curctx->current.color = COLOR4(color);
+	update_color(COLOR4(color));
 }
 
 
 void glColor4f (GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)
 {
-	pspgl_curctx->current.color = ((((int) (255.0 * CLAMPF(alpha))) << 24) |
-				       (((int) (255.0 * CLAMPF(blue))) << 16) |
-				       (((int) (255.0 * CLAMPF(green))) << 8) |
-					((int) (255.0 * CLAMPF(red))));
+	update_color((((int) (255.0 * CLAMPF(alpha))) << 24) |
+		     (((int) (255.0 * CLAMPF(blue))) << 16) |
+		     (((int) (255.0 * CLAMPF(green))) << 8) |
+		     ((int) (255.0 * CLAMPF(red))));
 }
 
 
@@ -24,18 +30,18 @@ void glColor3f (GLfloat red, GLfloat green, GLfloat blue)
 
 void glColor4ubv (const GLubyte *c)
 {
-	pspgl_curctx->current.color = (c[3] << 24) | (c[2] << 16) | (c[1] << 8) | c[0];
+	update_color((c[3] << 24) | (c[2] << 16) | (c[1] << 8) | c[0]);
 }
 
 
 void glColor3ub (GLubyte r, GLubyte g, GLubyte b)
 {
-	pspgl_curctx->current.color = 0xff000000 | (b << 16) | (g << 8) | b;
+	update_color(0xff000000 | (b << 16) | (g << 8) | r);
 }
 
  
 void glColor4ub (GLubyte r, GLubyte g, GLubyte b, GLubyte a)
 {
-	pspgl_curctx->current.color = (a << 24) | (b << 16) | (g << 8) | b;
+	update_color((a << 24) | (b << 16) | (g << 8) | r);
 }
 
