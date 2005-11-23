b2cca4d393786ab84195f79fc32fe658edb8f0ec
jsgf <none@none>
2005-11-23 17:42:14 +0000

    Fix glViewport and the coords passed to glScissor.  The hardware uses y=0 == top of the screen, but GL defines y=0 is at the bottom.  Also, correctly set up the screen offset of the viewport's corner.

--- glScissor.c
+++ glScissor.c
@@ -3,12 +3,19 @@
 
 void glScissor (GLint x, GLint y, GLsizei width, GLsizei height)
 {
+	int t,b,l,r;
+
 	pspgl_curctx->scissor_test.x = x;
 	pspgl_curctx->scissor_test.y = y;
 	pspgl_curctx->scissor_test.width = width;
 	pspgl_curctx->scissor_test.height = height;
 
-	sendCommandi(CMD_SCISSOR1, (y << 10) | x);
-	sendCommandi(CMD_SCISSOR2, (((y + height) - 1) << 10) | (((x + width) - 1)));
+	b = pspgl_curctx->draw->height - y;
+	t = b - height;
+	l = x;
+	r = x + width - 1;
+
+	sendCommandi(CMD_SCISSOR1, (t << 10) | l);
+	sendCommandi(CMD_SCISSOR2, ((b-1) << 10) | r);
 }
 
