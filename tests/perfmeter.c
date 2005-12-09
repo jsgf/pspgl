a0e1ee69cd5eadf445665b0a1834933cf0fc67db
jsgf <none@none>
2005-12-09 17:30:03 +0000

    Improve the perfmeter code to use a vertex array for the graph ticks, which reduces the graph-drawing's overhead.  Makes use of glPushAttrib to avoid upsetting the calling code's state as well.

--- tests/perfmeter.c
+++ tests/perfmeter.c
@@ -19,6 +19,11 @@ static long long now(void)
 
 static void showstats(float drawtime, float frametime, float queuewait)
 {
+	GLCHK(glPushAttrib(GL_ENABLE_BIT |
+			   GL_COLOR_BUFFER_BIT |
+			   GL_CURRENT_BIT |
+			   GL_TRANSFORM_BIT));
+
 	GLCHK(glMatrixMode(GL_MODELVIEW));
 	GLCHK(glPushMatrix());
 	GLCHK(glLoadIdentity());
@@ -40,6 +45,9 @@ static void showstats(float drawtime, float frametime, float queuewait)
 	GLCHK(glDisable(GL_TEXTURE_2D));
 	GLCHK(glEnable(GL_BLEND));
 	GLCHK(glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA));
+#if GL_PSP_vertex_blend
+	GLCHK(glDisable(GL_VERTEX_BLEND_PSP));
+#endif
 
 	/* draw graph background */
 	glColor4f(.25,.25,.25,.5);
@@ -78,16 +86,49 @@ static void showstats(float drawtime, float frametime, float queuewait)
 		glEnd();
 	}
 
-	glBegin(GL_LINES);
-	{
-		int i;
+	GLCHK(glPushClientAttrib(GL_CLIENT_VERTEX_ARRAY_BIT));
 
-		glColor4f(.5,.5,0,.5);
-		for(i = 0; i < 100; i++) {
-			glVertex2f(i / 1000., 0);
-			glVertex2f(i / 1000., i % 10 ? ((i % 5) ? .25 : .5) : .75);
+	GLCHK(glEnableClientState(GL_VERTEX_ARRAY));
+	GLCHK(glDisableClientState(GL_NORMAL_ARRAY));
+	GLCHK(glDisableClientState(GL_TEXTURE_COORD_ARRAY));
+	GLCHK(glDisableClientState(GL_COLOR_ARRAY));
+#if GL_PSP_vertex_blend
+	GLCHK(glDisableClientState(GL_WEIGHT_ARRAY_PSP));
+#endif
+
+	static GLuint bufferid = 0;
+#define NTICKS	100
+	if (bufferid == 0) {
+		int i;
+		struct vertex {
+			float x,y,z;
+		} ticks[NTICKS * 2];
+
+		GLCHK(glGenBuffersARB(1, &bufferid));
+
+		for(i = 0; i < NTICKS; i++) {
+			ticks[i*2+0].x = i / 1000.f;
+			ticks[i*2+0].y = 0.f;
+			ticks[i*2+0].z = 0.f;
+			ticks[i*2+1].x = i / 1000.f;
+			ticks[i*2+1].y = i % 10 ? ((i % 5) ? .25 : .5) : .75;
+			ticks[i*2+1].z = 0.f;
 		}
 
+		GLCHK(glBindBufferARB(GL_ARRAY_BUFFER_ARB, bufferid));
+		GLCHK(glBufferDataARB(GL_ARRAY_BUFFER_ARB, 
+				      sizeof(ticks), ticks,
+				      GL_STATIC_DRAW_ARB));
+	} else
+		GLCHK(glBindBufferARB(GL_ARRAY_BUFFER_ARB, bufferid));
+	
+	GLCHK(glVertexPointer(3, GL_FLOAT, 0, NULL+0));
+	GLCHK(glColor4f(.5,.5,0,.5));
+	GLCHK(glDrawArrays(GL_LINES, 0, NTICKS*2));
+
+	GLCHK(glPopClientAttrib());
+
+	glBegin(GL_LINES);
 		glColor3f(0,1,0);
 		glVertex2f(1./60, 0);
 		glVertex2f(1./60, 1);
@@ -99,8 +140,9 @@ static void showstats(float drawtime, float frametime, float queuewait)
 		glColor3f(1,0,0);
 		glVertex2f(1./15, 0);
 		glVertex2f(1./15, 1);
-	}
 	GLCHK(glEnd());
+
+	GLCHK(glPopAttrib());
 	
 #if GL_PSP_view_matrix
 	GLCHK(glMatrixMode(GL_VIEW_PSP));
