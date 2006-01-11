8306434cff9d96321b3afd6e4b96d7273acff494
jsgf <none@none>
2006-01-11 08:22:34 +0000

    Make blending match what the hardware seems to be doing a bit better. Some OpenGL blend factors are not available in hardware, but they seem to be pretty unusual ones.

--- glBlendFunc.c
+++ glBlendFunc.c
@@ -1,51 +1,59 @@
 #include "pspgl_internal.h"
 
 
-/* Blend Factor */
-#define GE_SRC_COLOR		0
-#define GE_ONE_MINUS_SRC_COLOR	1
-#define GE_SRC_ALPHA		2
-#define GE_ONE_MINUS_SRC_ALPHA	3
-#define GE_DST_ALPHA		4
-#define GE_ONE_MINUS_DST_ALPHA	5
-#define GE_DST_COLOR		0
-#define GE_ONE_MINUS_DST_COLOR	1
-#define GE_FIX			10
+/* Blend Src Factor */
+#define GE_SF_DST_COLOR			0
+#define GE_SF_ONE_MINUS_DST_COLOR	1
+#define GE_SF_SRC_ALPHA			2
+#define GE_SF_ONE_MINUS_SRC_ALPHA	3
+#define GE_SF_DST_ALPHA			4
+#define GE_SF_ONE_MINUS_DST_ALPHA	5
+#define GE_SF_FIX			10
+
+/* Blend Dst Factor */
+#define GE_DF_SRC_COLOR			0
+#define GE_DF_ONE_MINUS_SRC_COLOR	1
+#define GE_DF_SRC_ALPHA			2
+#define GE_DF_ONE_MINUS_SRC_ALPHA	3
+#define GE_DF_DST_ALPHA			4
+#define GE_DF_ONE_MINUS_DST_ALPHA	5
+#define GE_DF_FIX			10
 
 
 void glBlendFunc (GLenum sfactor, GLenum dfactor)
 {
-	unsigned int srcfunc, dstfunc;
+	unsigned int srcfact, dstfact;
 
 	switch (dfactor) {
 	case GL_ZERO:
 		sendCommandi(CMD_FIXEDCOL_DST, 0x000000);
-		dstfunc = GE_FIX;
+		dstfact = GE_DF_FIX;
 		break;
 	case GL_ONE:
 		sendCommandi(CMD_FIXEDCOL_DST, 0xffffff);
-		dstfunc = GE_FIX;
-		break;
-	case GL_DST_COLOR:
-		dstfunc = GE_DST_COLOR;
-		break;
-	case GL_ONE_MINUS_DST_COLOR:
-		dstfunc = GE_ONE_MINUS_DST_COLOR;
+		dstfact = GE_DF_FIX;
 		break;
 	case GL_SRC_ALPHA:
-		dstfunc = GE_SRC_ALPHA;
+		dstfact = GE_DF_SRC_ALPHA;
 		break;
 	case GL_ONE_MINUS_SRC_ALPHA:
-		dstfunc = GE_ONE_MINUS_SRC_ALPHA;
+		dstfact = GE_DF_ONE_MINUS_SRC_ALPHA;
 		break;
 	case GL_DST_ALPHA:
-		dstfunc = GE_DST_ALPHA;
+		dstfact = GE_DF_DST_ALPHA;
 		break;
 	case GL_ONE_MINUS_DST_ALPHA:
-		dstfunc = GE_ONE_MINUS_DST_ALPHA;
+		dstfact = GE_DF_ONE_MINUS_DST_ALPHA;
 		break;
 	case GL_SRC_COLOR:
+		dstfact = GE_DF_SRC_COLOR;
+		break;
 	case GL_ONE_MINUS_SRC_COLOR:
+		dstfact = GE_DF_ONE_MINUS_SRC_COLOR;
+		break;
+
+	case GL_DST_COLOR:
+	case GL_ONE_MINUS_DST_COLOR:
 	default:
 		GLERROR(GL_INVALID_ENUM);
 		return;
@@ -54,38 +62,40 @@ void glBlendFunc (GLenum sfactor, GLenum dfactor)
 	switch (sfactor) {
 	case GL_ZERO:
 		sendCommandi(CMD_FIXEDCOL_SRC, 0x000000);
-		srcfunc = GE_FIX;
+		srcfact = GE_SF_FIX;
 		break;
 	case GL_ONE:
 		sendCommandi(CMD_FIXEDCOL_SRC, 0xffffff);
-		srcfunc = GE_FIX;
+		srcfact = GE_SF_FIX;
 		break;
 	case GL_SRC_ALPHA:
-		srcfunc = GE_SRC_ALPHA;
+		srcfact = GE_SF_SRC_ALPHA;
 		break;
 	case GL_ONE_MINUS_SRC_ALPHA:
-		srcfunc = GE_ONE_MINUS_SRC_ALPHA;
+		srcfact = GE_SF_ONE_MINUS_SRC_ALPHA;
 		break;
-	case GL_SRC_COLOR:
-		srcfunc = GE_SRC_COLOR;
+	case GL_DST_COLOR:
+		srcfact = GE_SF_DST_COLOR;
 		break;
-	case GL_ONE_MINUS_SRC_COLOR:
-		srcfunc = GE_ONE_MINUS_SRC_COLOR;
+	case GL_ONE_MINUS_DST_COLOR:
+		srcfact = GE_SF_ONE_MINUS_DST_COLOR;
 		break;
 	case GL_DST_ALPHA:
-		srcfunc = GE_DST_ALPHA;
+		srcfact = GE_SF_DST_ALPHA;
 		break;
 	case GL_ONE_MINUS_DST_ALPHA:
-		srcfunc = GE_ONE_MINUS_DST_ALPHA;
+		srcfact = GE_SF_ONE_MINUS_DST_ALPHA;
 		break;
+
 	case GL_SRC_ALPHA_SATURATE:
-	case GL_DST_COLOR:
-	case GL_ONE_MINUS_DST_COLOR:
+	case GL_SRC_COLOR:
+	case GL_ONE_MINUS_SRC_COLOR:
 	default:
 		GLERROR(GL_INVALID_ENUM);
 		return;
 	}
 
-	__pspgl_context_writereg_masked(pspgl_curctx, CMD_BLEND_FUNC, (dstfunc << 4) | srcfunc, 0x0ff);
+	__pspgl_context_writereg_masked(pspgl_curctx, CMD_BLEND_FUNC,
+					(dstfact << 4) | srcfact, 0x0ff);
 }
 
